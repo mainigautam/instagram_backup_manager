ed29ec21cded7f961ddea82cdccca720d4e27c09
Gautam <gautammaini9@gmail.com>
2021-01-26 00:56:47 +0530

    client/server: Improvements *Last Access Backup Now Persistant *Delete Logic Improvement

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {useState,useEffect} from 'react'
 import Profile from './Profile'
 import Media from './Media'
 import Direct from './Direct'
@@ -7,10 +7,21 @@ import Post from './Components/Post.js'
 import Upload from './Upload.js'
 import {Route,Switch} from 'react-router-dom';
 const App = () => {
+    const [already , setAlready] = useState(false)
+    const fetchData = async  () => {
+        const result= await fetch('/existing');
+        const data = await result.json();
+        setAlready(data);
+    }
+    useEffect( ()=>{
+        fetchData();
+    },[]);
+    console.log(already);
     return (
         <>     
         <Switch>
-            <Route exact path='/' component={Upload} />
+            <Route exact path='/' component={ already ? Profile :Upload} />
+            <Route exact path='/upload' component={Upload}/>
             <Route exact path="/profile" component={Profile}/>
             <Route exact path='/photo/:id' component={Post}/>
             <Route exact path='/direct' component={Direct} />
