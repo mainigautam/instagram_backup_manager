1bfc0e125f5d4c5ab75a740795a08cb1e33ce36e
Gautam Maini <gautammaini9@gmail.com>
2022-01-27 01:40:06 +0530

    client: Improved Routing and Redirects

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,25 @@
 import React , {useState,useEffect} from 'react'
 import Profile from './Profile'
 import Media from './Media'
-import Direct from './Direct'
 import Direct2 from './Direct2'
-import Messages from './Components/Messages'
 import Messages2 from './Components/Messages2'
 import Post from './Components/Post.js'
 import instaLogo from './Components/instagram-new.png'
 import Upload from './Upload.js'
+import NoPage from './Components/NoPage'
 import {Route,Switch} from 'react-router-dom';
 import Reels from './Reels'
 const App = () => {
     const [already , setAlready] = useState(false);
-    const [loading , setLoading] = useState(true)
-    const [v2 , setV2] = useState(false);
+    const [loading , setLoading] = useState(false)
     const fetchData = async  () => {
         const result= await Promise.all([
-            fetch('/v2'),
             fetch('/existing')
         ]).then(function (responses) {
             return Promise.all(responses.map(function (response) {
                 return response.json();
         }));})
-        setAlready(result[1]);
-        setV2(result[0]);
+        setAlready(result[0]);
         setLoading(false);
     }
     useEffect( ()=>{
@@ -42,13 +38,13 @@ const App = () => {
                     <>     
                     <Switch>
                         <Route exact path='/' component={ already ? Profile :Upload} />
-                        <Route exact path='/upload' component={Upload}/>
-                        <Route exact path="/profile" component={Profile}/>
-                        <Route exact path='/photo/:id' component={Post}/>
-                        <Route exact path='/direct' component={v2 ? Direct2 : Direct} />
-                        <Route exact path='/direct/:id' component={v2 ? Messages2 : Messages} />
-                        <Route exact path='/reels' component={Reels} />
-                        <Route exact path='/media' component={Media} />
+                        <Route exact path='/profile' component={already?Profile:Upload}/>
+                        <Route exact path='/photo/:id' component={already? Post:Upload}/>
+                        <Route exact path='/direct' component={already?Direct2:Upload} />
+                        <Route exact path='/direct/:id' component={already?Messages2:Upload} />
+                        <Route exact path='/reels' component={already?Reels:Upload} />
+                        <Route exact path='/media' component={already?Media:Upload} />
+                        <Route component={NoPage}/>
                     </Switch>
                     </>
                 )
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './style/css/all.css'
+import './style/profile.css'
+import './style/navigator.css'
+import './style/inbox.css'
+import './style/messages.css'
 import {BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
