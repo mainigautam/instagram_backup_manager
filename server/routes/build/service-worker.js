dabfc0d38b9f6abe79bb435797cb8d55bfab1dce
Gautam Maini <gautammaini9@gmail.com>
2022-02-02 22:42:27 +0530

    server: update frontend bundle for useage

--- server/routes/build/service-worker.js
+++ server/routes/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91f82d46ad2d8f83915b465ed2855e89.js"
+  "/precache-manifest.5ac43b4cfc20cb6c8756900b40cac6e9.js"
 );
 
 self.addEventListener('message', (event) => {
