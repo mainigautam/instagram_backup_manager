f7edd3096d2d54a1ca2bbb17fad32a60c4bd7f9a
Gautam Maini <gautammaini9@gmail.com>
2022-08-01 15:33:00 +0530

    server : adapt to new format

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const profileRoute = (app, fs) => {
-    const dataPath = "../data/account_information/personal_information.json";
+    const dataPath = "../data/personal_information/personal_information.json";
     app.get("/user", (req, res) => {
         fs.readFile(dataPath, "utf8", (err, data) => {
           if (err) {
